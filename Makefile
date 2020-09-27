cc717772b8b653a286a4e95debdca1c6c73ba45c
Felipe Borges de Souza Chaves <a160049733@cm4.esw>
2020-09-27 18:55:22 -0300

    Adding script to collect data from runs with some changes in the number of bits
    Signed-off-by: Felipe Borges de Souza Chaves <a160049733@cm4.esw>

--- Makefile
+++ Makefile
@@ -5,10 +5,6 @@ bits=16
 all: compile run clean
 
 compile: 
-	OMP_NUM_THREADS=$(workers)
-
-	echo $(OMP_NUM_THREADS)
-
 	@gcc -o prog.out -I. -D NUM_WORKERS=$(workers) -D MODULO_NUM_BITS=$(bits) -fopenmp vow_with_hash.c
 
 run:
---
+++ run.sh
@@ -0,0 +1,13 @@
+# !bin/bash
+
+for worker in 1 2 4 8 12
+do
+  for nbits in 16
+  do
+    echo "Compiling with workers = $worker and bits = $nbits"
+    make compile workers=$worker bits=$nbits
+
+    echo "Make the run workers = $worker and bits = $nbits"
+    make run > example_$worker-$nbits.txt
+  done
+done
