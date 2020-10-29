7920e1b267712820256b08eca7da423d9dca7cf3
Felipe Borges <bumbleblo2013@gmail.com>
2020-10-29 15:27:30 -0300

    Adding error message
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 # !bin/bash
 
-for worker in 1 2 4
+for worker in 1 2 4 8 12
 do
-  for nbits in 16 20
+  for nbits in 20 24 28
   do
     echo "Make the run workers = $worker and bits = $nbits"
     make workers=$worker bits=$nbits>> experiment_$worker-$nbits.txt 2>&1 
+    status=$?
+
+    [ $status -eq 0 ] && echo "OK" || echo "ERROR"
   done
 done
